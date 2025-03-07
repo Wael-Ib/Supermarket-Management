c5f4967cfebd620cc4589996be005d0ff4d38a62
Wael Ibrahim <iwael1377@gmail.com>
2025-03-07 14:53:46 +0200

    using static repository to store data use the controller-model-view triangle to implement a page use tag helpers to create navigation links use tag helpers to create a html form handling httpPost request

--- WebApp/Models/Category.cs
+++ WebApp/Models/Category.cs
@@ -1,9 +1,14 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
-        public string Name { get; set; }=string.Empty;
-        public string Description { get; set; }= string.Empty;
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+
     }
 }
+

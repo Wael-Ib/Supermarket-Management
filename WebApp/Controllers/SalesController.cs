56bff119d5f0211b7dd6dd49e021343e60f3e38f
Wael Ibrahim <iwael1377@gmail.com>
2025-03-09 14:04:13 +0200

    implementing edit product creating product partialview implementing delete product creating partial view rendering the product list css-jquery to highlight selected product implementing sales form

---
+++ WebApp/Controllers/SalesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers
+{
+    public class SalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var salesViewModel = new SalesViewModel
+            {
+                Categories = CategoriesRepository.GetCategories()
+            };
+            return View(salesViewModel);
+        }
+        public IActionResult SellProductPartial(int productId)
+        {
+            var product = ProductsRepository.GetProductById(productId);
+            return PartialView("_SellProduct", product);
+        }
+        public IActionResult sell(SalesViewModel salesViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Sell the product
+
+                var prod = ProductsRepository.GetProductById(salesViewModel.SelectedProductId);
+                if (prod != null)
+                {
+                    TransactionsRepository.Add(
+                        "Cashier1",
+                        salesViewModel.SelectedProductId,
+                        prod.Name,
+                        prod.Price.HasValue ? prod.Price.Value : 0,
+                        prod.Quantity.HasValue ? prod.Quantity.Value : 0,
+                        salesViewModel.QuantityToSell);
+
+                    prod.Quantity -= salesViewModel.QuantityToSell;
+                    ProductsRepository.UpdateProduct(salesViewModel.SelectedProductId, prod);
+                }
+            }
+            var product = ProductsRepository.GetProductById(salesViewModel.SelectedProductId);
+            salesViewModel.SelectedCategoryId = (product?.CategoryId == null) ? 0 : product.CategoryId.Value;
+            salesViewModel.Categories = CategoriesRepository.GetCategories();
+            return View("Index", salesViewModel);
+        }
+    }
+}
